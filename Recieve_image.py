31888fd3714bb38b2ca0041db1db4c8b6e03aa70
ShaneThompson <shanethompson10@gmail.com>
2020-12-10 18:25:38 +0000

    Receive_image.py now receives jpg images from raspberry pi and saves them

--- Recieve_image.py
+++ Recieve_image.py
@@ -1,23 +1,26 @@
 import socket               # Import socket module
 
+counter = 0
 s = socket.socket()         # Create a socket object
-host = "localhost"          # Get local machine name
-port = 12345                 # Reserve a port for your service.
+host = "192.168.1.7" # Get local machine name
+port = 12345                # Reserve a port for your service.
+print (host)                # I wanted to see the hostname
 s.bind((host, port))        # Bind to the port
-f = open('image.jpg','wb')
+
 s.listen(5)                 # Now wait for client connection.
 print ("Waiting for client")
 while True:
     c, addr = s.accept()     # Establish connection with client.
     print ('Got connection from', addr)
     print ("Receiving...")
+    f = open('image' + str(counter)+ '.jpg', 'wb')
     l = c.recv(1024)
     while l:
-        print ("Receiving...")
         f.write(l)
         l = c.recv(1024)
     f.close()
     print ("Done Receiving")
     c.close()
+    counter += 1
 
 print ("All done")
